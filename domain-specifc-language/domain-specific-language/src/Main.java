6dcae5e4fa969cf21ef7197817f406a7e56e3479
arthurpedro <arthurpedroweb@gmail.com>
2019-06-25 09:37:51 -0300

    fix name

---
+++ domain-specifc-language/domain-specific-language/src/Main.java
@@ -0,0 +1,12 @@
+import Linguagem.SimpleSQL;
+
+public class Main {
+
+//     public static final String QUERY = "SELECT id nome FROM usuario";
+
+    public static final String QUERY = "INSERT usuario VALUES 4 Heloisa (81)996354295";
+
+    public static void main(String[] args) {
+        SimpleSQL.run(QUERY);
+    }
+}
